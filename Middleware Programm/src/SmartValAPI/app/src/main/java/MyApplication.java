dfe5a52de54e068a9cd4657fd13732c14e9429d8
Reimar Klammer <reimar.klammer@babos.co.at>
2017-03-14 15:04:33 +0100

    projektstruktur erweitert

---
+++ Middleware Programm/src/SmartValAPI/app/src/main/java/MyApplication.java
@@ -0,0 +1,13 @@
+import at.ac.fh.salzburg.smartmeter.access.DataAccess;
+
+/**
+ * Created by reimarklammer on 14.03.17.
+ */
+public class MyApplication {
+    public static void main(String[] args) {
+        System.out.println("Hallo Welt");
+
+
+        DataAccess access = new DataAccess(null,null);
+    }
+}
